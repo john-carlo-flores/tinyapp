410ea1a75b269c8b6034ff35d36be93fa369621a
JC Flores <tothenextcode@gmail.com>
2022-04-18 20:26:03 +0000

    Added get responses to /urls.json and /hello

--- express_server.js
+++ express_server.js
@@ -3,13 +3,21 @@ const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
+  "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body> Hello <b>World</b></body></html>\n");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
