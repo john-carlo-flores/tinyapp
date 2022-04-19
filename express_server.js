1f430db9a8b27e6252794c1571c91c99a588a42c
JC Flores <tothenextcode@gmail.com>
2022-04-19 17:14:16 +0000

    Added app.post for delete action to remove shortURL from urlDatabase

--- express_server.js
+++ express_server.js
@@ -44,6 +44,11 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL] === undefined) {
     res.statusCode = 404;
